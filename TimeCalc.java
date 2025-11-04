14e7985a4863b1602758f722d803b7dfee410b69
Dandanor <Dandanor2003@gmail.com>
2025-11-04 18:02:03 +0200

    did assignment 1

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,25 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int addmin = Integer.parseInt(args[1]);
+        int newhour= (hours + addmin/60)%24;
+        int newmin= minutes + addmin%60;
+        if (newmin>60){
+            newmin-=60;
+            newhour+=1;
+        }
+        if(newhour<10&&newmin<10){
+        System.out.println("0"+newhour+ ":0"+ newmin); 
+        }
+        else if (newhour<10&&newmin>=10){
+        System.out.println("0"+newhour+ ":"+ newmin); 
+        }
+        else if (newhour>=10&&newmin<10){
+        System.out.println(newhour+ ":0"+ newmin); 
+        }
+        else{
+        System.out.println(newhour+ ":"+ newmin);
+        }
     }
 }
