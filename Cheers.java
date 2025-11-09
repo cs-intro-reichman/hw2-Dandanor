a33bcf118dc51533d9b088004b72bf989a7eb2d9
Dandanor <Dandanor2003@gmail.com>
2025-11-09 22:09:05 +0200

    did assignment 3

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,27 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                char ch ;
+                String an= "AEFHILMNORSX";
+                int num = Integer.parseInt(args[1]);
+
+                for (int i=0; i<=word.length()-1; i++)
+                {
+                        ch=word.charAt(i);
+                        if(an.indexOf(ch)==-1)
+                        {
+                                System.out.println("Give me a "+ch+": "+ch+"!");
+                        }
+                        else
+                        {
+                                System.out.println("Give me an "+ch+": "+ch+"!");
+                        }
+                }
+                System.out.println("What does that spell?");
+                for (int i=1; i<=num;i++)
+                {
+                        System.out.println(word +"!!!");
+                }
         }
 }
