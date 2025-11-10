96d9e8d105b5950f2ca961052a69b1cdb1ea5d3b
Dandanor <Dandanor2003@gmail.com>
2025-11-10 21:57:29 +0200

    Changes to calcpi

--- CalcPi.java
+++ CalcPi.java
@@ -3,20 +3,20 @@ public class CalcPi {
 	public static void main(String [] args) {
 		int num = Integer.parseInt(args[0]);
 		double divisor = 3.0;
-		int count = 0;
+		int count = 1;
 		double aprox = 1.0;
-		while (count<=num)
+		while (count<num)
 		{
-			if (count%2==0){
-				aprox += 1/divisor;
+			if (count%2!=0){
+				aprox -= 1/divisor;
 			}
 			else{
-				aprox-= 1/divisor;
+				aprox += 1/divisor;
 			}
 			divisor+=2;
 			count++;
 		}
 		System.out.println("pi according to Java: " +Math.PI);
-		System.out.println("pi, approximated:     " +aprox);
+		System.out.println("pi, approximated:     " +aprox*4);
 	}
 }
